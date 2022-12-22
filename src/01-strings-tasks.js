f1496561b191ef430359f31ef989c2d6694f70e3
marteom <marteom@gmail.com>
2022-12-22 16:28:22 +0300

    feat: add code extractNameFromTemplate

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -65,8 +65,9 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, John Doe!' => 'John Doe'
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
-function extractNameFromTemplate(/* value */) {
-  throw new Error('Not implemented');
+function extractNameFromTemplate(value) {
+  const splitStr = value.split(', ');
+  return splitStr[1].substring(0, splitStr[1].length - 1);
 }
 
 
