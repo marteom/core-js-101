c5a561727a43d7f31959718c626392e7c7592b32
marteom <marteom@gmail.com>
2022-12-29 18:32:36 +0300

    feat: add code getPositivesCount

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -363,8 +363,8 @@ function get3TopItems(arr) {
  *   [ null, 1, 'elephant' ] => 1
  *   [ 1, '2' ] => 1
  */
-function getPositivesCount(/* arr */) {
-  throw new Error('Not implemented');
+function getPositivesCount(arr) {
+  return (arr.filter((element) => typeof (element) === 'number' && element > 0)).length;
 }
 
 /**
