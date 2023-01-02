16ed8e4f4985d6883019e19efb2891068a726266
marteom <marteom@gmail.com>
2023-01-02 10:26:00 +0300

    feat: add code timeSpanToString

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -74,8 +74,17 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(startDate, endDate) {
+  const milliseconds = endDate - startDate;
+
+  const seconds = Math.floor((milliseconds / 1000) % 60);
+  const minutes = Math.floor((milliseconds / 1000 / 60) % 60);
+  const hours = Math.floor((milliseconds / 1000 / 60 / 60) % 24);
+
+  const millisecondsPart = new Date(milliseconds).getMilliseconds();
+  const millisecondsVal = String(millisecondsPart).padStart(3, 0);
+
+  return `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}.${millisecondsVal}`;
 }
 
 
